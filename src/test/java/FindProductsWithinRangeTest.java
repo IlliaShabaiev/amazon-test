8663d3fef13765370d3c575a78ca5299f21b31e8
Illia Shabaiev <ilyashabaev2209@gmail.com>
2018-12-02 21:03:59 -0600

    Added new class & maven-compiler-plugin

---
+++ src/test/java/FindProductsWithinRangeTest.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class FindProductsWithinRangeTest {
+
+    @Test
+    public void findFirstFiveAirPads20To100dollars(){
+        System.out.println("I am a test");
+    }
+
+}
